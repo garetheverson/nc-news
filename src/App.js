28d7365b076a6c0cf719f3047929d10118966f99
Gareth Everson <gareth@garetheverson.com>
2022-04-21 15:16:42 +0100

    ncnews-05: Separate page for each topic
    ***ADDED***
    /components/SingleTopic - handles topics by url param
    
    ***CHANGED***
    components/Nav - programmatic retrieval of topics for menu updated for topic params. If new topics are added, they'll get added to nav bar automatically

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 // import Header from './components/Header';
 import Nav from './components/Nav';
 import Articles from './components/Articles';
+import SingleTopic from './components/SingleTopic';
 import Footer from './components/Footer';
+import { getArticles } from './utils/api';
 
 function App() {
+  // --- ARTICLES ---
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    getArticles().then((articlesFromApi) => {
+      setArticles(articlesFromApi.articles);
+    });
+  }, []);
+
+  // --- APP ---
   return (
     <div className='App'>
       {/* <Header /> */}
       <Nav />
       <Routes>
         <Route path='/' element={<Articles />}></Route>
+        <Route
+          path='/topics/:slug'
+          element={<SingleTopic articles={articles} />}
+        />
       </Routes>
       <Footer />
     </div>
---
+++ src/components/SingleTopic.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../App.css';
+import { useParams } from 'react-router-dom';
+
+const SingleTopic = ({ articles }) => {
+  const params = useParams();
+  const filterTopics = articles.filter(
+    (article) => article.topic === params.slug
+  );
+  return (
+    <main>
+      <h1 className='topic-heading'>{params.slug}</h1>
+      <ul className='article-list'>
+        {filterTopics.map((article) => {
+          const date = new Date(article.created_at);
+          const publishedDate = `${date.getDate()}/${
+            date.getMonth() + 1
+          }/${date.getFullYear()}`;
+          return (
+            <li key={article.article_id} className='shadow-lg'>
+              <h4 className='featured-post'>Featured post</h4>
+              <h4 className='previous-post'>Other posts</h4>
+              <h2>{article.title}</h2>
+              <p className='post-details'>
+                Topic:{' '}
+                {article.topic.charAt(0).toUpperCase() + article.topic.slice(1)}
+                {' / '}Author: {article.author}
+                {' / '}Votes: {article.votes}
+                {' / '}Published: {publishedDate}
+              </p>
+              <img
+                src={`https://picsum.photos/id/${article.article_id}/400/200?blur=1`}
+                alt={article.title}
+              ></img>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default SingleTopic;
